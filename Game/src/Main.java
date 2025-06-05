8a12f3436f390fca49f00bd67d6eb5eb4e041246
github Rion-Ishimaru <rion-ishimaru@kccs.co.jp>
2025-06-05 15:46:32 +0900

    メインのname,hp,mpを追加しました

--- Game/src/Main.java
+++ Game/src/Main.java
@@ -2,8 +2,24 @@
 public class Main {
 
 	public static void main(String[] args) {
+		int hp =500;
+		int mp = 50;
+		String name = "tukusi";
 		
-
+		information(name , hp , mp);
+		System.out.println();
+		attack(name , hp);
 	}
-
+	public static void information(String name , int hp , int mp) {
+		System.out.println("名前：" + name);
+		System.out.println("HP：" + hp);
+		System.out.println("MP：" + mp);
+	}
+	
+	public static void attack( String name , int mp) {
+		System.out.println( name + "勇者はは棍棒を振った");
+		System.out.println("ダメージ30あたえた！");
+		mp -= 3;
+	}
+	
 }
